efdc21d11a6e3138817c452ef2ab23b839b6ae09
Idhibhat Pankam <marginpankam@gmail.com>
2025-03-25 15:26:29 +0700

    8-10

--- code/Week10_OpsStars/airflow/dsde_simplebash.py
+++ code/Week10_OpsStars/airflow/dsde_simplebash.py
@@ -1,10 +1,9 @@
 from airflow.utils.dates import days_ago
 from airflow import DAG
-
 from airflow.operators.bash import BashOperator
 
-dag = DAG('dsde_simplebash', start_date=days_ago(1))
+# declare a DAG
+with DAG(dag_id='dsde_simplebash', start_date=days_ago(1)):
 
-echo = BashOperator(task_id='echo_template', bash_command='echo "run_id = {{ run_id }} and ds = {{ ds }}"', dag=dag)
-
-echo
+	# declare a TASK
+	echo = BashOperator(task_id='echo_template', bash_command='echo "run_id = {{ run_id }} and ds = {{ ds }}"')
