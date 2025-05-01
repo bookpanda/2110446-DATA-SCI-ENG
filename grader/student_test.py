a1b9020d8805e1e6d60b6a40964409b68d27e412
Idhibhat Pankam <marginpankam@gmail.com>
2025-05-01 23:09:56 +0700

    2

---
+++ grader/student_test.py
@@ -0,0 +1,16 @@
+def Q1():
+    """
+	[Grader: YES] 522*4043 = ?
+    """
+    # Hint: int [ex. 999]
+    return 522 * 4043
+
+
+def Q2():
+    '''
+	[Grader: NO] What is the course code for this course?
+    '''
+    # Hint: string [ex. "A"]
+    return "2110446"
+
+# Submit this file to Grader: DSDE_Test_Ans 
